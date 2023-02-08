60210ab7273ebc1d81e257c85f25ada33910e375
Walber <walber@santanatextiles.com>
2023-02-08 16:17:42 -0300

    Projeto Passe Geral criado

---
+++ src/main/java/com/santanatextiles/PassegeralApplication.java
@@ -0,0 +1,13 @@
+package com.santanatextiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassegeralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassegeralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santanatextiles/PassegeralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santanatextiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassegeralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
